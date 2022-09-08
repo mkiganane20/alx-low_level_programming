c6dd6897c5d0bb9ac926629451d9683985535bbf
mkiganane20 <mkiganane20@github.com>
2022-09-08 12:49:58 -0700

    Commit 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+##include <stdio.h>
+
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proer grammar, but the outcome is a iece of art,\n");
+	return (0);
+}
