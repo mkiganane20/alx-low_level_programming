9ae36286b413bbbd69508e68033cf790df7dc97e
mkiganane20 <mkiganane20@github.com>
2022-09-08 14:09:52 -0700

    Commit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
-
 /**
- * main - Prints a text
- *
- * Return: Always 0 (Success)
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
-	printf("with proer grammar, but the outcome is a iece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
 }
