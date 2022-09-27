55a93f57b18f754917ad52c77c779f0b366ac78a
mkiganane20 <mkiganane20@github.com>
2022-09-27 00:46:38 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H                                                                                                                     
+#define MAIN_H                                                                                                                     
+
+/**                                                                                                                                
+ * File: main.h                                                                                                                    
+ * Auth: sam tech (samuel birhanu)                                                                                                 
+ * description: Header file containing prototypes for all functions                                                                
+ *       used in the 0x06-pointers_arrays_strings directory.                                                                       
+ */                                                          
